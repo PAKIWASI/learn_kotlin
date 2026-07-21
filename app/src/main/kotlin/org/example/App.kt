f3922b3d7f801edf6c56570f8d069bb84e2cc2b8
PAKIWASI <ullahwasi26@gmail.com>
2026-07-22 02:43:16 +0500

    setup treesiter discrepencies

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,17 +1,37 @@
 package org.example
 
 class App {
-
     val greeting: String
-        get() {
-            return "whe fhdf"
-        }
+        get() = "ehrke"
+
+    fun hello() {
+
+
+    }
+}
+
+
+fun foo(
+    a: Int,
+    b: Int
+) {
+    println(a + b)
+
+    println(
+        Math.PI
+
+    )
 }
 
 fun main() {
+    var a = App()
 
-    println(App().greeting)
+    println(a.greeting)
+
+    foo(
+        5,
+        10
+    )
 
     println("what the fuc")
-
 }
