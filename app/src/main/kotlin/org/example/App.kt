b6790a2854f9e52b0037b618134b518724c0d23e
PAKIWASI <ullahwasi26@gmail.com>
2026-07-22 02:07:08 +0500

    initial commit

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,17 @@
+package org.example
+
+class App {
+
+    val greeting: String
+        get() {
+            return "whe fhdf"
+        }
+}
+
+fun main() {
+
+    println(App().greeting)
+
+    println("what the fuc")
+
+}
